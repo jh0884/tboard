075b12650ad26851e91ba18001e5eafeda9fb343
jh0884 <wlgh0884@gmail.com>
2020-09-08 21:52:30 +0900

    Initial commit

---
+++ src/main/java/com/tboard/example/tboard/TtboardApplication.java
@@ -0,0 +1,13 @@
+package com.tboard.example.tboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tboard/example/tboard/TtboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tboard.example.tboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
