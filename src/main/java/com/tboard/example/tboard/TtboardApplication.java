a41379c9d197c9b9f9c5440d36ea84c74b090e90
jh0884 <wlgh0884@gmail.com>
2020-09-09 10:51:58 +0900

    2020-09-09 diquest

--- src/main/java/com/tboard/example/tboard/TtboardApplication.java
+++ src/main/java/com/tboard/example/tboard/TtboardApplication.java
@@ -2,7 +2,9 @@ package com.tboard.example.tboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TtboardApplication {
 
